2b85d1747204f0a36214aa000f5abac4e1932e85
Kalle Wik <kwikdev@gmail.com>
2022-11-20 15:39:03 -0800

    Push game to website at public/game directory

---
+++ packages/nouns-webapp/public/game/TemplateData/web3Connect.js
@@ -0,0 +1,13 @@
+function WalletConnect() {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+
+        // connect popup
+        ethereum.enable();
+
+        window.ethereum.on("accountsChanged", function () {
+            location.reload();
+        });
+
+    }
+}
---
+++ packages/nouns-webapp/public/game/network.js
@@ -0,0 +1,7 @@
+/*
+Used to set the network: https://chainlist.org/
+1 Mainnet
+4 Rinkeby
+*/
+
+window.web3ChainId = 1;
