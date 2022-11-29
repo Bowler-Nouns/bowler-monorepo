b7a40806b79475fd4021218c69c1416e78008193
yanuar-ar <yanuar.ar@gmail.com>
2022-11-29 17:15:14 +0700

    add loading to game

--- packages/nouns-webapp/src/pages/Play/index.tsx
+++ packages/nouns-webapp/src/pages/Play/index.tsx
@@ -1,13 +1,24 @@
 import { Unity, useUnityContext } from 'react-unity-webgl';
+import loadingNoun from '../../assets/loading-skull-noun.gif';
+import Image from 'react-bootstrap/Image';
 
 const PlayPage = () => {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded } = useUnityContext({
     loaderUrl: 'game/Build/nouns-bowling-game.loader.js',
     dataUrl: 'game/Build/nouns-bowling-game.data',
     frameworkUrl: 'game/Build/nouns-bowling-game.framework.js',
     codeUrl: 'game/Build/nouns-bowling-game.wasm',
   });
 
-  return <Unity unityProvider={unityProvider} />;
+  return (
+    <>
+      {isLoaded === false && (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Image src={loadingNoun} alt="Loading noun" />
+        </div>
+      )}
+      <Unity unityProvider={unityProvider} />
+    </>
+  );
 };
 export default PlayPage;
